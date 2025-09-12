1210ad307b8786d1ba049ca4fdb7c98c8c7bb268
Andres Callejas <andres.callejas@iudigital.edu.co>
2025-09-12 00:59:05 +0000

    actualizacion yml

--- src/prueba_etl/main.py
+++ src/prueba_etl/main.py
@@ -10,11 +10,11 @@ from extractor import Extractor
 if __name__ == "__main__":
     # DataFrame de ejemplo
     ruta_db = "/workspaces/iidsd_20252_1/src/prueba_etl/static/db/database.sqlite"
-    url = "shivamshinde1904/weather-data2000-2023"
+    url = "uom190346a/global-coffee-health-dataset"
     database = Database(ruta_db)
     extractor = Extractor()
     dataset = extractor.download_dataset_zip(url) 
     csv_dir =extractor.extract_zip_files(dataset)
     df = extractor.create_csv(csv_dir)
     print(df.head(2))
-    database.create_table(df, "weather_data")
+    database.create_table(df, "coffee_health")
